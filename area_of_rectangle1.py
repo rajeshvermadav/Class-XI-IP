6f63360508dd4e312ee2f70b04a7afb32c5bfaf6
Rajesh verma <68262909+rajeshvermadav@users.noreply.github.com>
2022-07-14 13:21:14 +0530

    Add files via upload

---
+++ area_of_rectangle1.py
@@ -0,0 +1,7 @@
+# calculate area of Rectangle
+length = int(input("Enter length of rectangle:-"))
+breadth = float(input("Enter breadth of rectangle:-"))
+
+area = length * breadth
+
+print("Area of Rectangle:", area)
---
+++ arithmeticop_demo.py
@@ -0,0 +1,20 @@
+# Arithmetic Operators Demo
+x=6
+y=9
+a = int(input("Enter First Number:-"))
+b = int(input("Enter Second Number:-"))
+
+print("Sum of two numbers:-", a+b)
+
+print("Subtract two Numbers:-", a-b)
+
+print("Multiplication of two numbers:-", a*b)
+
+print("Division of two numbers:-",a/b)
+
+print("Floor Divsion:-", a//b)          #Truncate the fractional part
+
+print("Returns the remainder:-", a%b)   #Modulus operatos shows remainder
+
+print("Exponent of number:", a**b)   #Returns base raised to power exponent
+
